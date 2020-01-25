5af29674d7016a8484600c8cfd56ec8a92bb9b20
Sahil <sahilbondre@gmail.com>
2020-01-25 17:33:55 +0530

    add react router and dashboard ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./assets/lib/scss/black-dashboard-react.scss";
+import LandingPage from "./pages/LandingPage";
 
 class App extends Component {
   state = {};
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
---
+++ client/src/pages/LandingPage/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>LandingPage</p>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;

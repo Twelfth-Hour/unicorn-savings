558228ee1d209c975b4693f9972369374a9fbad9
AemieJ <aemie.j@hotmail.co.uk>
2020-01-25 20:29:59 +0530

    change pet/set to create efficient communication b/w firebase and react

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ admin.initializeApp({
 let db = admin.firestore();
 
 // Add user details in firebase store
-/* eslint-disable-next-line no-unused-vars */
 app.post("/user/set", (req, res) => {
   let userModel = req.body;
   db.collection("users")
@@ -37,12 +36,23 @@ app.post("/user/set", (req, res) => {
 });
 
 //Add pet details in firebase store
-/* eslint-disable-next-line no-unused-vars */
 app.post("/pet/set", (req, res) => {
-  const petModel = req.body;
-  let petRef = db.collection("pets").doc();
-  /* eslint-disable-next-line no-unused-vars */
-  let petUser = petRef.set(petModel, { merge: true });
+  let petModel = req.body;
+  db.collection("pets")
+    .doc(petModel.email)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        // new pet save to db
+        db.collection("pets")
+          .doc(petModel.email)
+          .set(petModel, { merge: true });
+        res.send(petModel);
+      } else {
+        // existing user get data from db
+        res.send(doc.data());
+      }
+    });
 });
 
 //Send pet details back to client side
@@ -51,31 +61,12 @@ app.post("/pet/get/:email", (req, res) => {
   /* eslint-disable-next-line no-unused-vars */
   let petQuery = db
     .collection("pets")
-    .where("owner", "==", email)
+    .doc(email)
     .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        res.send(doc.data());
-      });
-    });
-});
-
-
-// Update pet details based on the xp
-/* eslint-disable-next-line no-unused-vars */
-app.post("/pet/:email", (req, res) => {
-    let email = req.params.email;
-    let petLevel = db.collection("pets").where("owner", "==", email).get()
-    .then(snapshot => {
-        snapshot.forEach(doc => {
-            let data = doc.data();
-            if (req.body.xp >= (data.level * 5)) {
-                let newLevel = req.body.level;
-                db.collection("pets").where("owner", "==", email).set({
-                    level: 4
-                })
-            }            
-        });
+    .then(doc => {
+      if (doc.exists) {
+        res.send(doc.data);
+      }
     });
 });
 

4ed8cd9029d62d124c5fed73ec1caa6f567d5c2e
Sahil <sahilbondre@gmail.com>
2020-01-25 16:49:29 +0530

    add models

---
+++ models/pet.model.js
@@ -0,0 +1,8 @@
+module.exports = {
+  id: "skfjlsdk",
+  owner: "email of owner",
+  name: "Poopo",
+  hp: 100,
+  xp: 0,
+  level: 0
+};
---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+module.exports = {
+  id: "hash",
+  name: "John Doe",
+  email: "j@gmail.com",
+  is_new: true,
+  pet: "hash",
+  target: 40000,
+  daily: 40,
+  savings: 1000
+};

6a4a97f4a7c0537f9a35be62203f75a109838825
Sahil <sahilbondre@gmail.com>
2020-01-25 16:29:35 +0530

    initial setup: added eslint, prettier, express, cra

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  useTabs: false, // Indent lines with tabs instead of spaces.
+  printWidth: 100, // Specify the length of line that the printer will wrap on.
+  tabWidth: 2, // Specify the number of spaces per indentation-level.
+  singleQuote: false // Use single quotes instead of double quotes.
+};

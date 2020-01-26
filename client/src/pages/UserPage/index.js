d0f9b70ed7601fa12f2f0fa0d18a1e4e88f63022
Sahil <sahilbondre@gmail.com>
2020-01-26 07:35:33 +0530

    fix level change bug

--- client/src/pages/UserPage/index.js
+++ client/src/pages/UserPage/index.js
@@ -24,92 +24,13 @@ import badge2 from "../../assets/svg/seven.svg";
 import badge3 from "../../assets/svg/hero.svg";
 const chartColor = "#FFFFFF";
 
-const data = canvas => {
-  var ctx = canvas.getContext("2d");
-
-  var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-  gradientStroke.addColorStop(0, "#80b6f4");
-  gradientStroke.addColorStop(1, chartColor);
-
-  var gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
-  gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
-  gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
-  return {
-    labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
-    datasets: [
-      {
-        label: "Money Saved",
-        borderColor: "#f96332",
-        pointBorderColor: "#FFF",
-        pointBackgroundColor: "#f96332",
-        pointBorderWidth: 2,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 1,
-        pointRadius: 4,
-        fill: true,
-        backgroundColor: gradientFill,
-        borderWidth: 2,
-        data: [42, 48, 43, 55, 53, 4, 80]
-      }
-    ]
-  };
-};
-const options = {
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  tooltips: {
-    bodySpacing: 4,
-    mode: "nearest",
-    intersect: 0,
-    position: "nearest",
-    xPadding: 10,
-    yPadding: 10,
-    caretPadding: 10
-  },
-  responsive: 1,
-  scales: {
-    yAxes: [
-      {
-        display: 0,
-        ticks: {
-          display: false
-        },
-        gridLines: {
-          zeroLineColor: "transparent",
-          drawTicks: false,
-          display: false,
-          drawBorder: false
-        }
-      }
-    ],
-    xAxes: [
-      {
-        display: 0,
-        ticks: {
-          display: false
-        },
-        gridLines: {
-          zeroLineColor: "transparent",
-          drawTicks: false,
-          display: false,
-          drawBorder: false
-        }
-      }
-    ]
-  },
-  layout: {
-    padding: { left: 0, right: 0, top: 15, bottom: 15 }
-  }
-};
-
 class UserPage extends Component {
   state = {
     modal: false,
     nextLevel: 1000,
     leaderboard: [],
-    rank: 1
+    rank: 1,
+    history: [0, 0, 0, 0, 0, 0, 0]
   };
   async componentDidMount() {
     if (!this.props.user.auth) {
@@ -119,7 +40,10 @@ class UserPage extends Component {
     const response = await res.text();
     const data = JSON.parse(response);
     this.props.setPet(data);
-    this.setState({ nextLevel: (5 * (this.props.pet.level + 1) * (this.props.pet.level + 2)) / 2 });
+    this.setState({
+      nextLevel: (5 * (this.props.pet.level + 1) * (this.props.pet.level + 2)) / 2,
+      history: this.props.pet.history
+    });
     let a = await post(`/leaderboard/${this.props.user.email}`);
     const b = await a.json();
     this.setState({ leaderboard: b.array, rank: b.ownerRank });
@@ -139,6 +63,85 @@ class UserPage extends Component {
     });
   };
   render() {
+    const data = canvas => {
+      var ctx = canvas.getContext("2d");
+
+      var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+      gradientStroke.addColorStop(0, "#80b6f4");
+      gradientStroke.addColorStop(1, chartColor);
+
+      var gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+      gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+      gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
+      return {
+        labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: "Money Saved",
+            borderColor: "#f96332",
+            pointBorderColor: "#FFF",
+            pointBackgroundColor: "#f96332",
+            pointBorderWidth: 2,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 1,
+            pointRadius: 4,
+            fill: true,
+            backgroundColor: gradientFill,
+            borderWidth: 2,
+            data: this.state.history
+          }
+        ]
+      };
+    };
+    const options = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        bodySpacing: 4,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest",
+        xPadding: 10,
+        yPadding: 10,
+        caretPadding: 10
+      },
+      responsive: 1,
+      scales: {
+        yAxes: [
+          {
+            display: 0,
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              zeroLineColor: "transparent",
+              drawTicks: false,
+              display: false,
+              drawBorder: false
+            }
+          }
+        ],
+        xAxes: [
+          {
+            display: 0,
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              zeroLineColor: "transparent",
+              drawTicks: false,
+              display: false,
+              drawBorder: false
+            }
+          }
+        ]
+      },
+      layout: {
+        padding: { left: 0, right: 0, top: 15, bottom: 15 }
+      }
+    };
     return (
       <div className="UserPage">
         <div id="hero">
@@ -219,7 +222,13 @@ class UserPage extends Component {
               <h2>Vitals</h2>
               <h4>Savings Today: 30₹</h4>
               <h4>XP for next level: {this.state.nextLevel - this.props.pet.xp}</h4>
-              <h4>Average Saving for Last Week: 45.8₹</h4>
+              <h4>
+                Average Saving for Last Week:{" "}
+                {Math.round(
+                  this.state.history.reduce((a, b) => a + b, 0) / this.state.history.length
+                )}
+                ₹
+              </h4>
               <h4>Your Rank: {this.state.rank}</h4>
             </Col>
           </Row>
